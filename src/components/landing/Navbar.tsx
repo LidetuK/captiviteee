9a2f0e8854a60a009d3b6af828712f44b1385497
Lidetu Ketema <lidetuketema495@gmail.com>
2025-02-28 00:17:37 +0300

    Update Navbar.tsx
    Signed-off-by: Lidetu Ketema <lidetuketema495@gmail.com>

--- src/components/landing/Navbar.tsx
+++ src/components/landing/Navbar.tsx
@@ -40,7 +40,7 @@ interface NavbarProps {
   logo?: string;
 }
 
-const defaultLogo = "/CAPTIVITE-X/assets/logo.png";
+const defaultLogo = "/CAPTIVITE-X/public/assets/logo.png";
 
 
 
@@ -221,12 +221,7 @@ const Navbar = ({
         {/* Logo */}
         <div className="flex items-center">
           <div onClick={() => navigate("/")} className="cursor-pointer">
-          <LazyImage
-  src={logo}
-  alt="Captivite Logo"
-  className="w-40 h-40 object-contain"  // Increased size
-  fallback="https://api.dicebear.com/7.x/initials/svg?seed=C"
-/>
+         
 
           </div>
           <span
